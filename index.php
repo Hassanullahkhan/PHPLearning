80daa145a42c3d89bf5d4f70f83fc10c0799331c
hassanullahkhan <hasaankhaan456@gmail.com>
2025-03-18 02:55:21 +0500

    some styling changes

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP with Git</title>
 </head>
-<body>
+<body style="background-color:rgb(96, 84, 84)";>
     <head> I like PHP and am now learning git</head>
+    <h1 style="color: rgb(195, 173, 173)";>
     <?php echo "I like Pizza"?>
     <?php
     $guysName = "Jensins";
@@ -15,6 +16,6 @@
     $sonName = "Brody";
     
     echo "my name is  $guysName  I have a wife, Her name is  $wifesName  and I have 1 son and 1 daughter both their names are  $sonName  and  $daughterName "
-    ?>
+    ?></h1>
 </body>
 </html>
