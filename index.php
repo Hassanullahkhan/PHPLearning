d2a6b98a1baed291dd691a2272c6516a38774264
hassanullahkhan <hasaankhaan456@gmail.com>
2025-03-18 02:39:26 +0500

    changes been made in code

--- index.php
+++ index.php
@@ -8,5 +8,13 @@
 <body>
     <head> I like PHP and am now learning git</head>
     <?php echo "I like Pizza"?>
+    <?php
+    $guysName = "Jensins";
+    $wifesName = "Lilly Jensins";
+    $daughterName = "Aisha";
+    $sonName = "Brody";
+    
+    echo "my name is  $guysName  I have a wife, Her name is  $wifesName  and I have 1 son and 1 daughter both their names are  $sonName  and  $daughterName "
+    ?>
 </body>
 </html>
