5182d822f1503ec03a524c29eab4d2d4e30e3637
hassanullahkhan <hasaankhaan456@gmail.com>
2025-03-18 02:06:36 +0500

    my first commit with git bash using PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP with Git</title>
+</head>
+<body>
+    <head> I like PHP and am now learning git</head>
+    <?php echo "I like Pizza"?>
+</body>
+</html>
